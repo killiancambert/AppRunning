01c35a9505a8d47a99cfff70f685e764f27dc9f1
Claude Christian Letembet Ambily <claudechristianletembetambily@MacBook-Pro-de-Claude.local>
2020-02-04 16:57:18 +0100

    initConnexion

---
+++ components/Connexion.js
@@ -0,0 +1,60 @@
+// Components/connexion.js
+import React from 'react'
+import { View, TextInput, Button,Image } from 'react-native'
+
+class Connexion extends React.Component {
+    render() {
+        return (
+            <View style={styles.view}>
+
+            {/* View for the logo */}
+
+                <View style={{ height: 100, width: 100, backgroundColor: 'red' }} >
+                <TextInput placeholder='LOGO'/>
+                </View>
+
+            {/* View for the  text */}
+
+            <View style={{ height: 250, width:250 , backgroundColor: 'blue' }} >
+                <TextInput placeholder='Connexion and little text'/>
+                </View>
+
+            {/* View for the form*/}
+
+            <View style={{ height: 100, width: 100, backgroundColor: 'red' }} >
+               
+            <TextInput placeholder='Email'/>
+            <TextInput placeholder='Mot de passe'/>
+
+            </View>
+
+              {/* View for the button */}
+
+            <View >
+               
+        <Button title='Se Connecter' onPress={() => {}}/>
+            </View>
+
+     
+    
+      </View>
+        )
+    }
+}
+
+const styles = {
+
+    view: {
+        flex:1,
+        backgroundColor :'#F96155',
+        justifyContent: 'center',
+        alignItems : 'center',
+      },
+
+    placeholder :{
+        justifyContent : 'center'
+    }
+
+}
+
+export default Connexion
