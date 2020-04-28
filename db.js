9eb9d923ad2e1cf54d2161427cb044b242a8d837
killiancambert <killian.cambert@gmail.com>
2020-04-28 08:49:38 +0200

    change db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import '@firebase/firestore';
 const firebaseConfig = {
   projectId: "apprunning-8c3f3",
   apiKey: "AIzaSyDD83EjGr38HJVopMrRucwE4Zuq1Ob-duY",
-  authDomain: "apprunning-8c3f3.firebaseapp.com",
+  authDomain: "apprunning-8c3f3.firebaseio.com",
   databaseURL: "https://apprunning-8c3f3.firebaseio.com",
   storageBucket: "apprunning-8c3f3.appspot.com"
 };
